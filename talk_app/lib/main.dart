c9ce3b0c0bd8794b2a34bd8812e26260ebe5c6c4
Yisroel Rosenblat <srulsterer@gmail.com>
2024-04-12 12:43:02 -0400

    added basic auth support

--- talk_app/lib/main.dart
+++ talk_app/lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:talk_app/firebase_options.dart';
 import 'package:talk_app/pages/ChatSelector.dart';
 import 'package:talk_app/pages/Settings.dart';
 import 'package:talk_app/pages/Account.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:talk_app/pages/SignIn.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
   runApp(const MyApp());
 }
 
@@ -37,10 +50,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   int _page = 0;
+  User? _user;
 
   void setPage(int page) {
     setState(() {
       _page = page;
+    });
+  }
+
+  void _setUser(User? user) {
+    setState(() {
+      _user = user;
     });
   }
 
@@ -52,38 +72,50 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    Widget page;
-    switch (_page) {
-      case 0:
-        page = ChatSelector();
-      case 1:
-        page = Settings();
-      case 2:
-        page = Account();
-      default:
-        page = Center(child: Text("error"));
-    }
+    final FirebaseAuth _auth = FirebaseAuth.instance;
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _page,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.settings), label: "Settings"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_rounded), label: "account")
-        ],
-        onTap: (value) {
-          setPage(value);
-        },
-      ),
-      body: page,
-    );
+    return StreamBuilder(
+        stream: _auth.authStateChanges(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.data == null) {
+            return const SignIn();
+          }
+          var currentUser = userSnapshot.data;
+          print(currentUser);
+
+          Widget page;
+          switch (_page) {
+            case 0:
+              page = ChatSelector();
+            case 1:
+              page = Settings();
+            case 2:
+              page = Account(auth: _auth, user: currentUser!);
+            default:
+              page = Center(child: Text("error"));
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(widget.title),
+              centerTitle: true,
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _page,
+              items: [
+                BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), label: "Settings"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.account_circle_rounded), label: "account")
+              ],
+              onTap: (value) {
+                setPage(value);
+              },
+            ),
+            body: page,
+          );
+        });
   }
 }
--- talk_app/lib/pages/Account.dart
+++ talk_app/lib/pages/Account.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Account extends StatelessWidget {
-  const Account({super.key});
+  final User user;
+  final FirebaseAuth auth;
+
+  const Account({super.key, required this.user, required this.auth});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.blue,
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(user.displayName ?? "Anonymous"),
+          Text(user.uid),
+          ElevatedButton(onPressed: auth.signOut, child: Text("log out"))
+        ],
+      ),
     );
   }
 }
