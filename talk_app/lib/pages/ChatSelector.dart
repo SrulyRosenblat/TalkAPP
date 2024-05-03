101a1fef6c6dd9dde5220b3ea6f5aaf1e2295ade
Yisroel Rosenblat <srulsterer@gmail.com>
2024-05-03 09:22:00 -0400

    fixed up the audio and extracted build message for reuse.

--- talk_app/lib/pages/ChatSelector.dart
+++ talk_app/lib/pages/ChatSelector.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:talk_app/database_middleware.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'Chat.dart'; 
+import 'Chat.dart';
 
 class ChatSelector extends StatelessWidget {
   final User user;
@@ -27,7 +27,7 @@ class ChatSelector extends StatelessWidget {
               const SizedBox(height: 8),
               DropdownButtonFormField<String>(
                 isExpanded: true,
-                icon: const Icon (
+                icon: const Icon(
                   Icons.arrow_drop_down_circle,
                   color: Color(0xFF7AA7FF),
                 ),
@@ -35,8 +35,14 @@ class ChatSelector extends StatelessWidget {
                 onChanged: (String? newValue) {
                   language = newValue!;
                 },
-                items: <String>['English', 'Spanish', 'French', 'German', 'Hebrew', 'Ukrainian']
-                    .map<DropdownMenuItem<String>>((String value) {
+                items: <String>[
+                  'English',
+                  'Spanish',
+                  'French',
+                  'German',
+                  'Hebrew',
+                  'Ukrainian'
+                ].map<DropdownMenuItem<String>>((String value) {
                   return DropdownMenuItem<String>(
                     value: value,
                     child: Text(value),
@@ -87,28 +93,36 @@ class ChatSelector extends StatelessWidget {
         itemBuilder: (context, index) {
           final chat = chats[index];
           return Container(
-            decoration: BoxDecoration(
-              color: const Color(0xFF7AA7FF), // Blue background color
-              borderRadius: BorderRadius.circular(8), // Rounded rectangle corners
-            ),
-            margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
-            child: ListTile(
-            title: Text(chat['title'] ?? 'No title', style: const TextStyle(color: Colors.white, fontSize: 24)), // Providing a fallback value for null
-            subtitle: Text(chat['subtitle'] ?? 'No subtitle', style: const TextStyle(color: Colors.white)), // Providing a fallback value for null
-            onTap: () {
-              if (chat['chatId'] != null) { // Checking for null before navigating
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => Chat(chatId: chat['chatId']!),
-                  ),
-                );
-              }
-            },
-          ));
+              decoration: BoxDecoration(
+                color: const Color(0xFF7AA7FF), // Blue background color
+                borderRadius:
+                    BorderRadius.circular(8), // Rounded rectangle corners
+              ),
+              margin: const EdgeInsets.symmetric(vertical: 6, horizontal: 16),
+              child: ListTile(
+                title: Text(chat['title'] ?? 'No title',
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 24)), // Providing a fallback value for null
+                subtitle: Text(chat['subtitle'] ?? 'No subtitle',
+                    style: const TextStyle(
+                        color: Colors
+                            .white)), // Providing a fallback value for null
+                onTap: () {
+                  if (chat['chatId'] != null) {
+                    // Checking for null before navigating
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => Chat(chatId: chat['chatId']!),
+                      ),
+                    );
+                  }
+                },
+              ));
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _createChat(context),
+        onPressed: () => createChat('en', 'e', 's'),
         tooltip: 'New Chat',
         shape: const CircleBorder(),
         child: const Icon(
--- talk_app/lib/pages/Favorites.dart
+++ talk_app/lib/pages/Favorites.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:talk_app/database_middleware.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 import 'dart:async';
+import 'package:talk_app/widgets/BuildMessage.dart';
+// ...
 
 class Favorites extends StatefulWidget {
   final User user;
@@ -15,71 +16,11 @@ class Favorites extends StatefulWidget {
 
 class FavoritesState extends State<Favorites> {
   late Future<Map<String, dynamic>> _favoritesFuture;
-  late FlutterTts flutterTts;
 
   @override
   void initState() {
     super.initState();
     _favoritesFuture = getFavorites(widget.user.uid);
-    flutterTts = FlutterTts();
-  }
-
-  void _speak(String text) async {
-    await flutterTts.speak(text);
-  }
-
-  Widget buildMessage(String translatedTexts, String originalTexts, String sounds, String roles) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: roles == "assistant" ? MainAxisAlignment.start : MainAxisAlignment.end,
-        children: [
-          if (roles == "user") 
-            IconButton(
-              icon: Icon(Icons.volume_up),
-              color: Colors.black,
-              onPressed: () => _speak(translatedTexts),
-            ),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.7,
-            ),
-            decoration: BoxDecoration(
-              color: roles == "assistant" ? Color(0xFF8A8AFF) : Color(0xFF7AA7FF),
-              borderRadius: BorderRadius.circular(20),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 3,
-                  spreadRadius: 1,
-                  color: Colors.black.withOpacity(0.1),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  translatedTexts,
-                  style: TextStyle(fontSize: 16.0, color: Colors.white),
-                ),
-                SizedBox(height: 4),
-                Text(
-                  originalTexts,
-                  style: TextStyle(fontSize: 14.0, color: Colors.white70),
-                ),
-              ],
-            ),
-          ),
-          if (roles == "assistant") 
-            IconButton(
-              icon: Icon(Icons.volume_up),
-              color: Colors.black,
-              onPressed: () => _speak(translatedTexts),
-            ),
-        ],
-      ),
-    );
   }
 
   @override
@@ -101,14 +42,16 @@ class FavoritesState extends State<Favorites> {
               child: Text('Error fetching favorites: ${snapshot.error}'),
             );
           } else {
-            List<dynamic> translatedTexts = snapshot.data?['translatedTexts'] ?? [];
+            List<dynamic> translatedTexts =
+                snapshot.data?['translatedTexts'] ?? [];
             List<dynamic> originalTexts = snapshot.data?['originalTexts'] ?? [];
             List<dynamic> sounds = snapshot.data?['sounds'] ?? [];
             List<dynamic> roles = snapshot.data?['roles'] ?? [];
             return ListView.builder(
               itemCount: translatedTexts.length,
               itemBuilder: (context, index) {
-                return buildMessage(translatedTexts[index], originalTexts[index], sounds[index], roles[index]);
+                return buildMessage(translatedTexts[index],
+                    originalTexts[index], sounds[index], roles[index], context);
               },
             );
           }
