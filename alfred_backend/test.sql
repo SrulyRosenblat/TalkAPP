a11b3cfe273acd19d35ed381cba06bbea156c41b
Yisroel Rosenblat <srulsterer@gmail.com>
2024-05-03 09:19:16 -0400

    fixed up the audio and extracted build message for reuse

---
+++ alfred_backend/test.sql
@@ -0,0 +1,8 @@
+INSERT INTO AppUser (UserID) VALUES ('12312312132421');
+
+SELECT * FROM AppUser
+
+
+INSERT INTO Chat (ChatID,UserID,NativeLang,ForeignLang,ChatName) VALUES ('12312312132421','chat-1','English','hebrew','hebrew-english chat lol');
+
+
