506c9006b2dce81c9d310ee30c210c106fc2059b
Yisroel Rosenblat <srulsterer@gmail.com>
2024-05-07 17:13:37 -0400

    tashMerge branch 'AccountPage-CustomizePage'

---
+++ talk_app/lib/pages/SettingModel/ThemeWidget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:talk_app/pages/SettingModel/Theme.dart';
+
+class ChangeThemeButtonWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Switch.adaptive(
+      value:themeProvider.isDarkMode,
+      onChanged:(value){},
+    );
+  }
+}
