4bda735282da11ca1ce5cbd31724ad5dcf7ae39e
Yisroel Rosenblat <srulsterer@gmail.com>
2024-05-07 17:15:34 -0400

    got rid of cloudfunctions

--- talk_app/lib/pages/SettingModel/ThemeWidget.dart
+++ talk_app/lib/pages/SettingModel/ThemeWidget.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:talk_app/pages/SettingModel/Theme.dart';
 
-class ChangeThemeButtonWidget extends StatelessWidget{
+class ChangeThemeButtonWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
 
     return Switch.adaptive(
-      value:themeProvider.isDarkMode,
-      onChanged:(value){},
+      value: themeProvider.isDarkMode,
+      onChanged: (value) {},
     );
   }
 }
